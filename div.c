ae4a2a673fbff271c3da64c5b87065688ed6db91
klupzor <lopezgand@gmail.com>
2019-05-16 00:44:33 +0000

    create opcode div

---
+++ div.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * div - divides the second top element of the stack by the top element of the stack.
+ * @head: double pointer to head node.
+ * @line: line number.
+ */
+void div(stack_t **head, unsigned int line)
+{
+	int val1, val2;
+	stack_t *tmp;
+
+	if (count(head) < 2)
+		print_error(line, "can't div, stack too short");
+
+	val1 = (*head)->n;
+	val2 = (*head)->next->n;
+	if (val1 == 0)
+		print_error(line, "division by zero");
+	(*head)->next->n = val2 / val1;
+	(*head)->next->prev = NULL;
+	tmp = (*head)->next;
+
+	free(*head);
+	*head = tmp;
+
+}
